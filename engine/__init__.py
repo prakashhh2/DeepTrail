20393ddb78f6926548bdca9c817932980776a5ce
Prakash Kusari <prakashkusari0@gmail.com>
2026-09-10 21:18:14 -0500

    Expose crawler export helpers

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,6 +1,14 @@
 """DeepTrain crawler engine."""
 
 from engine.crawler import crawl, crawl_sync
+from engine.exporter import crawl_response_dict, write_crawl_json
 from engine.models import CrawlRequest, CrawlResponse
 
-__all__ = ["CrawlRequest", "CrawlResponse", "crawl", "crawl_sync"]
+__all__ = [
+    "CrawlRequest",
+    "CrawlResponse",
+    "crawl",
+    "crawl_sync",
+    "crawl_response_dict",
+    "write_crawl_json",
+]
